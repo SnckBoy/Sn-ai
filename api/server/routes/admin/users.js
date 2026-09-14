29fed377c95370d7d8513723d6f1054eef5b17cc
SnckBoy <feecly1@gmail.com>
2026-09-14 15:51:28 +0545

    fix: initialize Snck admin router through factory

--- api/server/routes/admin/users.js
+++ api/server/routes/admin/users.js
@@ -4,7 +4,7 @@ const { SystemCapabilities } = require('@librechat/data-schemas');
 const { requireCapability } = require('~/server/middleware/roles/capabilities');
 const { requireJwtAuth } = require('~/server/middleware');
 const db = require('~/models');
-const adminSnck = require('./snck');
+const createAdminSnckRouter = require('./snck');
 
 const router = express.Router();
 
@@ -24,7 +24,7 @@ router.use(requireJwtAuth, requireAdminAccess);
 
 router.get('/', requireReadUsers, handlers.listUsers);
 router.get('/search', requireReadUsers, handlers.searchUsers);
-router.use('/snck', adminSnck);
+router.use('/snck', createAdminSnckRouter());
 // router.delete('/:id', requireManageUsers, handlers.deleteUser);
 
 module.exports = router;
